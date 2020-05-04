31643c59326d8183e647b391aaf24558311f1d33
Stuart Ross <deepfunk9@hotmail.com>
2020-05-04 17:20:46 +0100

    end of first lesson

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -1,28 +1,18 @@
 import React from 'react';
-import logo from './../../favicon.ico';
 import './App.css';
+import BusinessList from './../BusinessList/BusinessList';
+import SearchBar from './../SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br></br>
-        <br></br>
-        <p>
-          Stay Focussed Stu
-        </p>
-        <a
-          className="App-link"
-          href="https://www.rabbitohs.com.au/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Rabbitohs
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList /> 
+      </div>
+    );
+  }
 }
 
 export default App;
--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './SearchBar.css';
-import { render } from 'react-dom';
 const sortByOptions = {
     'Best Match': 'best_match',
     'Highest Rated': 'rating',
@@ -12,14 +11,35 @@ class SearchBar extends React.Component {
         return Object.keys(sortByOptions).map(sortByOption => {
         //Object.keys Creates an array of the keys of an object
         //array.map() creates an array by going over the passed in array.
-            let sortByOptionValue =sortByOptions[sortByOption]; 
+            let sortByOptionValue = sortByOptions[sortByOption]; 
             //sortByOptionValue is the value from the key. "best_match"
             //sortByOptions is the object.
             //sortByOption: First key passed in from keys. "Best Match"
-            <li key={sortByOptionValue}>{sortByOption}</li>
+            return <li key={sortByOptionValue}>{sortByOption}</li>
             //key is required so react doesn't get lost
             //The text that will be displayed will be "Best Match" for the first iteration
         }); 
-
     }
+    //use this.renderSortByOptions() not <SearchBar />
+    render () {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                    {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a href="www.#.com">Let's Go</a>
+                </div>
+            </div>
+        )
+    }
+
 }
+
+export default SearchBar;
